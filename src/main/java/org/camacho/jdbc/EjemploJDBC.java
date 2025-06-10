7a014f0768a0e874685c23a3f8377fca4146c86f
Luxius42 <josemi8397@gmail.com>
2025-06-10 22:03:58 +0200

    Manejo de errores

--- src/main/java/org/camacho/jdbc/EjemploJDBC.java
+++ src/main/java/org/camacho/jdbc/EjemploJDBC.java
@@ -10,11 +10,14 @@ public class EjemploJDBC {
         String user = "root";
         String password = "admin";
 
+        Connection con = null; //Se declara fuera para tener una variable global y así poder usarlo luego en el finally
+        Statement st = null;
+        ResultSet rs = null;
         try {
 
-            Connection con = DriverManager.getConnection(url, user, password);
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from productos");
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("select * from productos2");
 
             try {
                 while (rs.next()) {
@@ -24,15 +27,25 @@ public class EjemploJDBC {
                     System.out.println("Fecha de inserción: " + rs.getDate(4));
                 }
             } catch (SQLException e) {
-                throw new RuntimeException(e);
+                System.out.println("Error al obtener datos: "  + e.getMessage().toUpperCase());
+               // throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error a la hora de conectar a la base de datos: " + e.getMessage().toUpperCase());
+           //throw new RuntimeException("Error conectando a la base de datos: ", e);
+        }finally {
+
+            try {
+                System.out.println("Cerrando conexion...");
+                rs.close();
+                st.close();
+                con.close();
+            } catch (SQLException e) {
+                System.out.println("Error al cerrar el conexion: " + e.getMessage().toUpperCase());
+                //throw new RuntimeException(e);
             }
 
-            rs.close();
-            st.close();
-            con.close();
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
 
 
